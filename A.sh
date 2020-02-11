9b74d9d51817cdefbe2c0b8066920d8cbe6a0230
avdevil <60883474+avdevil@users.noreply.github.com>
2020-02-11 21:32:56 +0530

    Update A.sh

--- A.sh
+++ A.sh
@@ -1,18 +1,18 @@
 #coded by avhacker
 #Avinash
-Clear
+clear
 hours=`date |cut -c12-13`
-If [ $hours-le 12 ]
-Then
+if [ $hours-le 12 ]
+then
 echo "good morning"
 else
 If [ $hours -le 16 ]
 Then
-echo"good afternoon"
+echo "good afternoon"
 elif [ $hours-le 20 ]
 then
-echo"good evening"
-Elie
-echo"good night"
+echo "good evening"
+elie
+echo "good night"
 fi
 fi
