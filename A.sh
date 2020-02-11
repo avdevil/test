eadf8852e070a706f5231da7828304b7e6465d3d
avdevil <60883474+avdevil@users.noreply.github.com>
2020-02-11 21:29:47 +0530

    Update A.sh

--- A.sh
+++ A.sh
@@ -1,6 +1,18 @@
 #coded by avhacker
 #Avinash
-{
-printf "\e[1;93m[!]you were hacked by devil.\n\e[0m"    
-printf"[!]poda potta."
-}
+Clear
+hours=`date |cut -c12-13`
+If [ $hours-le 12 ]
+Then
+echo "good morning"
+else
+If [ $hours -le 16 ]
+Then
+echo"good afternoon"
+elif [ $hours-le 20 ]
+then
+echo"good evening"
+Elie
+echo"good night"
+fi
+fi
